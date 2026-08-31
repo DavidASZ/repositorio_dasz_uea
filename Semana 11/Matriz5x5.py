7bb6765e51fd56c718a184c6dde569dc41a5dd29
DavidASZ <davidszambrano@gmail.com>
2026-08-30 19:51:16 -0500

    Tarea Semana 10 matriz 5x5

---
+++ Semana 11/Matriz5x5.py
@@ -0,0 +1,15 @@
+matriz = [[0 for _ in range(5)] for _ in range(5)]
+
+# Ingreso de los 25 valores
+for i in range(5):
+    for j in range(5):
+        valor = int(input(f"Ingrese el valor para la posición [{i}][{j}]: "))
+        matriz[i][j] = valor
+
+# Mostrar la matriz
+print("\nMatriz ingresada:")
+
+for i in range(5):
+    for j in range(5):
+        print(matriz[i][j], end="\t")
+    print()
